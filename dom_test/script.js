5e7f956c29d9ab33cda0bb4f4ef9fc51d755188b
Bednyakov <bednyakov99@gmail.com>
2023-11-11 13:00:49 +0300

    мелкие правки

--- dom_test/script.js
+++ dom_test/script.js
@@ -9,46 +9,6 @@ let answeredQuestions = {};
 let questionNumber = 0;
 let correctAnswers = 0;
 
-function displayCorrectAnswer(questionText, correctAnswer, explanation) {
-    let questionBlocks = document.querySelectorAll('.question-block');
-    for (let questionBlock of questionBlocks) {
-        if (questionBlock.querySelector('.question').textContent === questionText) {
-            let correctAnswerElement = document.createElement('div');
-            correctAnswerElement.classList.add('correct-answer');
-            correctAnswerElement.innerHTML = "Правильный ответ: " + correctAnswer + "<br><small>" + explanation + "</small>";
-            questionBlock.appendChild(correctAnswerElement);
-            break;
-        }
-    }
-}
-
-function createQuestionBlock() {
-    let questionBlock = document.createElement('div');
-    questionBlock.classList.add('question-block');
-
-    let answerBlock = document.createElement('div');
-    answerBlock.classList.add('answer-block');
-
-    let quizContainer = document.getElementById('quiz-container');
-    quizContainer.appendChild(questionBlock);
-    quizContainer.appendChild(answerBlock);
-
-    questionBlock.onclick = function () {
-        let questionElement = questionBlock.querySelector('.question');
-        if (!questionElement.classList.contains('selectable')) {
-            return;
-        }
-
-        let questionText = questionElement.textContent;
-        if (answeredQuestions[questionText]) {
-            displayCorrectAnswer(questionText, answeredQuestions[questionText].answer, answeredQuestions[questionText].explanation);
-            questionElement.classList.remove('selectable');
-        }
-    };
-
-    return { questionBlock, answerBlock };
-}
-
 function displayQuestion() {
     if (questions.length === 0) {
         document.getElementById('result').classList.remove('hidden');
@@ -90,7 +50,6 @@ function displayQuestion() {
             answerElement.classList.add(isCorrect ? 'correct' : 'incorrect');
             answeredQuestions[currentQuestion.question] = { answer: currentQuestion.answers[currentQuestion.correct], explanation: currentQuestion.explanation };
 
-            // Выключаем все ответы после выбора одного
             let allAnswers = this.parentElement.querySelectorAll('.answer');
             allAnswers.forEach(el => {
                 el.classList.add('disabled');
@@ -118,6 +77,46 @@ function displayQuestion() {
 
         answerBlock.appendChild(answerElement);
     });
+}
+
+function displayCorrectAnswer(questionText, correctAnswer, explanation) {
+    let questionBlocks = document.querySelectorAll('.question-block');
+    for (let questionBlock of questionBlocks) {
+        if (questionBlock.querySelector('.question').textContent === questionText) {
+            let correctAnswerElement = document.createElement('div');
+            correctAnswerElement.classList.add('correct-answer');
+            correctAnswerElement.innerHTML = "Правильный ответ: " + correctAnswer + "<br><small>" + explanation + "</small>";
+            questionBlock.appendChild(correctAnswerElement);
+            break;
+        }
+    }
+}
+
+function createQuestionBlock() {
+    let questionBlock = document.createElement('div');
+    questionBlock.classList.add('question-block');
+
+    let answerBlock = document.createElement('div');
+    answerBlock.classList.add('answer-block');
+
+    let quizContainer = document.getElementById('quiz-container');
+    quizContainer.appendChild(questionBlock);
+    quizContainer.appendChild(answerBlock);
+
+    questionBlock.onclick = function () {
+        let questionElement = questionBlock.querySelector('.question');
+        if (!questionElement.classList.contains('selectable')) {
+            return;
+        }
+
+        let questionText = questionElement.textContent;
+        if (answeredQuestions[questionText]) {
+            displayCorrectAnswer(questionText, answeredQuestions[questionText].answer, answeredQuestions[questionText].explanation);
+            questionElement.classList.remove('selectable');
+        }
+    };
+
+    return { questionBlock, answerBlock };
 }
 
 function shuffleArray(array) {

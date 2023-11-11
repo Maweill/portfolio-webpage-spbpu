1695db14e8142484c28a9933b6351c106e4473e6
Bednyakov <bednyakov99@gmail.com>
2023-11-11 13:20:17 +0300

    изменил структуру кода

--- dom_test/script.js
+++ dom_test/script.js
@@ -1,132 +1,135 @@
-const questions = [
-    { question: "А когда с человеком может произойти дрожемент?", answers: ["Когда он влюбляется", "Когда он идет шопиться", "Когда он слышит смешную шутку", "Когда он боится, пугается"], correct: 3, explanation: "Лексема «дрожемент» имплицирует состояние крайнего напряжения и страха: «У меня всегда дрожемент в ногах, когда копы подходят»." },
-    { question: "Говорят, Антон заовнил всех. Это еще как понимать?", answers: ["Как так, заовнил? Ну и хамло. Кто с ним теперь дружить-то будет?", "Антон очень надоедливый и въедливый человек, всех задолбал", "Молодец, Антон, всех победил!", "Нет ничего плохого в том, что Антон тщательно выбирает себе друзей"], correct: 2, explanation: "Термин «заовнить» заимствован из английского языка, он происходит от слова own и переводится как «победить», «завладеть», «получить»" },
-    { question: "А фразу «заскамить мамонта» как понимать?", answers: ["Разозлить кого-то из родителей", "Увлекаться археологией", "Развести недотепу на деньги", "Оскорбить пожилого человека"], correct: 2, explanation: "Заскамить мамонта — значит обмануть или развести на деньги. Почему мамонта? Потому что мошенники часто выбирают в жертвы пожилых людей (древних, как мамонты), которых легко обвести вокруг пальца." },
-    { question: "Кто такие бефефе?", answers: ["Вши?", "Милые котики, такие милые, что бефефе", "Лучшие друзья", "Люди, которые не держат слово"], correct: 2, explanation: "Бефефе — это лучшие друзья. Этакая аббревиатура от английского выражения best friends forever." },
+const quizData = [
+    {
+        q: "А когда с человеком может произойти дрожемент?",
+        a: ["Когда он влюбляется", "Когда он идет шопиться", "Когда он слышит смешную шутку", "Когда он боится, пугается"],
+        c: 3,
+        e: "Лексема «дрожемент» имплицирует состояние крайнего напряжения и страха: «У меня всегда дрожемент в ногах, когда копы подходят»."
+    },
+    {
+        q: "Говорят, Антон заовнил всех. Это еще как понимать?",
+        a: ["Как так, заовнил? Ну и хамло. Кто с ним теперь дружить-то будет?", "Антон очень надоедливый и въедливый человек, всех задолбал", "Молодец, Антон, всех победил!", "Нет ничего плохого в том, что Антон тщательно выбирает себе друзей"],
+        c: 2,
+        e: "Термин «заовнить» заимствован из английского языка, он происходит от слова own и переводится как «победить», «завладеть», «получить»"
+    },
+    {
+        q: "А фразу «заскамить мамонта» как понимать?",
+        a: ["Разозлить кого-то из родителей", "Увлекаться археологией", "Развести недотепу на деньги", "Оскорбить пожилого человека"],
+        c: 2,
+        e: "Заскамить мамонта — значит обмануть или развести на деньги. Почему мамонта? Потому что мошенники часто выбирают в жертвы пожилых людей (древних, как мамонты), которых легко обвести вокруг пальца."
+    },
+    {
+        q: "Кто такие бефефе?",
+        a: ["Вши?", "Милые котики, такие милые, что бефефе", "Лучшие друзья", "Люди, которые не держат слово"],
+        c: 2,
+        e: "Бефефе — это лучшие друзья. Этакая аббревиатура от английского выражения best friends forever."
+    },
 ];
 
-let answeredQuestions = {};
-let questionNumber = 0;
-let correctAnswers = 0;
+let answered = {};
+let qNum = 0;
+let correctNum = 0;
 
 function displayQuestion() {
-    if (questions.length === 0) {
+    if (quizData.length === 0) {
         document.getElementById('result').classList.remove('hidden');
-        document.getElementById('result').textContent = `Вопросы закончились. Правильных ответов: ${correctAnswers} из ${questionNumber}`;
-
-        let questionElements = document.querySelectorAll('.question');
-        for (let questionElement of questionElements) {
-            questionElement.classList.add('selectable');
+        document.getElementById('result').textContent = `Вопросы закончились. Правильных ответов: ${correctNum} из ${qNum}`;
+        let qElements = document.querySelectorAll('.question');
+        for (let qElement of qElements) {
+            qElement.classList.add('selectable');
         }
-        displayResultChart(correctAnswers, questionNumber);
+        showResultChart(correctNum, qNum);
         return;
     }
-
-    questionNumber++;
-    let { questionBlock, answerBlock } = createQuestionBlock();
-
-    let randomIndex = Math.floor(Math.random() * questions.length);
-    let currentQuestion = questions.splice(randomIndex, 1)[0];
-    let correctAnswer = currentQuestion.answers[currentQuestion.correct];
-
-    shuffleArray(currentQuestion.answers);
-
-    let newCorrectIndex = currentQuestion.answers.findIndex(answer => answer === correctAnswer);
-
-    questionBlock.innerHTML = `<div class="questionHeader">Вопрос ${questionNumber}</div> <div class="question">${currentQuestion.question}</div>`;
-    answerBlock.innerHTML = "";
-
-    currentQuestion.answers.forEach((answer, index) => {
-        let answerElement = document.createElement('div');
-        answerElement.classList.add('answer', 'selectable');
-        answerElement.textContent = answer;
-        answerElement.onclick = function () {
+    qNum++;
+    let { qBlock, aBlock } = createQBlock();
+    let randomIdx = Math.floor(Math.random() * quizData.length);
+    let currentQ = quizData.splice(randomIdx, 1)[0];
+    let correctA = currentQ.a[currentQ.c];
+    shuffle(currentQ.a);
+    let newCorrectIdx = currentQ.a.findIndex(answer => answer === correctA);
+    qBlock.innerHTML = `<div class="questionHeader">Вопрос ${qNum}</div> <div class="question">${currentQ.q}</div>`;
+    aBlock.innerHTML = "";
+    currentQ.a.forEach((answer, index) => {
+        let aElement = document.createElement('div');
+        aElement.classList.add('answer', 'selectable');
+        aElement.textContent = answer;
+        aElement.onclick = function () {
             if (this.classList.contains('disabled')) {
                 return;
             }
-
-            let isCorrect = index === newCorrectIndex;
-            answerElement.classList.add('disabled');
-            answerElement.classList.add(isCorrect ? 'correct' : 'incorrect');
-            answeredQuestions[currentQuestion.question] = { answer: currentQuestion.answers[currentQuestion.correct], explanation: currentQuestion.explanation };
-
+            let isCorrect = index === newCorrectIdx;
+            aElement.classList.add('disabled');
+            aElement.classList.add(isCorrect ? 'correct' : 'incorrect');
+            answered[currentQ.q] = { answer: currentQ.a[currentQ.c], explanation: currentQ.e };
             let allAnswers = this.parentElement.querySelectorAll('.answer');
             allAnswers.forEach(el => {
                 el.classList.add('disabled');
                 el.classList.remove('selectable');
             });
-
             if (isCorrect) {
-                correctAnswers++;
-                let correctAnswerElement = document.createElement('div');
-                correctAnswerElement.classList.add('correct-answer');
-                correctAnswerElement.innerHTML = "<small>" + currentQuestion.explanation + "</small>";
-                answerBlock.appendChild(correctAnswerElement);
+                correctNum++;
+                let correctAElement = document.createElement('div');
+                correctAElement.classList.add('correct-answer');
+                correctAElement.innerHTML = "<small>" + currentQ.e + "</small>";
+                aBlock.appendChild(correctAElement);
             }
-
             setTimeout(() => {
-                answerBlock.classList.add('slide-out');
-                answerBlock.addEventListener('animationend', () => {
+                aBlock.classList.add('slide-out');
+                aBlock.addEventListener('animationend', () => {
                     displayQuestion();
-                    answerBlock.classList.add('hidden');
-                    let questionElement = questionBlock.querySelector('.question');
-                    questionElement.classList.add(isCorrect ? 'correct-question' : 'incorrect-question');
+                    aBlock.classList.add('hidden');
+                    let qElement = qBlock.querySelector('.question');
+                    qElement.classList.add(isCorrect ? 'correct-question' : 'incorrect-question');
                 }, { once: true });
             }, 2000);
         };
-
-        answerBlock.appendChild(answerElement);
+        aBlock.appendChild(aElement);
     });
 }
 
-function displayCorrectAnswer(questionText, correctAnswer, explanation) {
-    let questionBlocks = document.querySelectorAll('.question-block');
-    for (let questionBlock of questionBlocks) {
-        if (questionBlock.querySelector('.question').textContent === questionText) {
-            let correctAnswerElement = document.createElement('div');
-            correctAnswerElement.classList.add('correct-answer');
-            correctAnswerElement.innerHTML = "Правильный ответ: " + correctAnswer + "<br><small>" + explanation + "</small>";
-            questionBlock.appendChild(correctAnswerElement);
-            break;
-        }
-    }
-}
-
-function createQuestionBlock() {
-    let questionBlock = document.createElement('div');
-    questionBlock.classList.add('question-block');
-
-    let answerBlock = document.createElement('div');
-    answerBlock.classList.add('answer-block');
-
+function createQBlock() {
+    let qBlock = document.createElement('div');
+    qBlock.classList.add('question-block');
+    let aBlock = document.createElement('div');
+    aBlock.classList.add('answer-block');
     let quizContainer = document.getElementById('quiz-container');
-    quizContainer.appendChild(questionBlock);
-    quizContainer.appendChild(answerBlock);
-
-    questionBlock.onclick = function () {
-        let questionElement = questionBlock.querySelector('.question');
-        if (!questionElement.classList.contains('selectable')) {
+    quizContainer.appendChild(qBlock);
+    quizContainer.appendChild(aBlock);
+    qBlock.onclick = function () {
+        let qElement = qBlock.querySelector('.question');
+        if (!qElement.classList.contains('selectable')) {
             return;
         }
-
-        let questionText = questionElement.textContent;
-        if (answeredQuestions[questionText]) {
-            displayCorrectAnswer(questionText, answeredQuestions[questionText].answer, answeredQuestions[questionText].explanation);
-            questionElement.classList.remove('selectable');
+        let qText = qElement.textContent;
+        if (answered[qText]) {
+            displayCorrectAnswer(qText, answered[qText].answer, answered[qText].explanation);
+            qElement.classList.remove('selectable');
         }
     };
-
-    return { questionBlock, answerBlock };
+    return { qBlock, aBlock };
 }
 
-function shuffleArray(array) {
+function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-function displayResultChart(correct, total) {
+function displayCorrectAnswer(qText, correctA, explanation) {
+    let qBlocks = document.querySelectorAll('.question-block');
+    for (let qBlock of qBlocks) {
+        if (qBlock.querySelector('.question').textContent === qText) {
+            let correctAElement = document.createElement('div');
+            correctAElement.classList.add('correct-answer');
+            correctAElement.innerHTML = "Правильный ответ: " + correctA + "<br><small>" + explanation + "</small>";
+            qBlock.appendChild(correctAElement);
+            break;
+        }
+    }
+}
+
+function showResultChart(correct, total) {
     const ctx = document.getElementById('resultChart').getContext('2d');
     const chart = new Chart(ctx, {
         type: 'doughnut',
@@ -151,6 +154,5 @@ function displayResultChart(correct, total) {
         }
     });
 }
-
 
 displayQuestion();
